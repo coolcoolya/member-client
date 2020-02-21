3fba77860f79bd27bc4d03c08f2669a3e602bba7
wangya <731716125@qq.com>
2020-02-21 17:19:20 +0800

    通过修改端口号，注册多个会员服务。测试订单服务通过restTemplate方式调用会员服务的时候，实现本地负载均衡。

--- src/main/java/com/example/memberclient/controller/MemberController.java
+++ src/main/java/com/example/memberclient/controller/MemberController.java
@@ -1,13 +1,17 @@
 package com.example.memberclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @RequestMapping("/getMember")
     public String getMember(){
-        return "this is member";
+        return "this is member,serverPort: "+serverPort;
     }
 }
